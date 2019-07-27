367f7c013dacbb3e5071e355040fde603b5614f8
jeff-tabios <jefftabios1@gmail.com>
2019-07-28 02:47:26 +0800

    Added stories view model tests

--- JTStoriesTests/StoriesViewModelTests.swift
+++ JTStoriesTests/StoriesViewModelTests.swift
@@ -7,22 +7,47 @@
 //
 
 import XCTest
+@testable import JTStories
 
 class StoriesViewModelTests: XCTestCase {
 
-    var sut = StoryListViewModel(api: MockAPI())
+    var sut = StoriesViewModel(api: MockAPI())
     
     func test_startAndget1Page() {
         sut.refresh()
-        XCTAssertEqual(sut.storyViewModels.value.count, 20)
+        XCTAssertEqual(sut.storyViewModels.count, 10)
     }
     
     func test_get2Pages() {
         sut.refresh()
-        XCTAssertEqual(sut.storyViewModels.value.count, 20)
+        XCTAssertEqual(sut.storyViewModels.count, 10)
         
         sut.getNextPage()
-        XCTAssertEqual(sut.storyViewModels.value.count, 40)
+        XCTAssertEqual(sut.storyViewModels.count, 20)
+    }
+    
+    func test_get2PagesThenRefresh() {
+        sut.refresh()
+        XCTAssertEqual(sut.storyViewModels.count, 10)
+        
+        sut.getNextPage()
+        XCTAssertEqual(sut.storyViewModels.count, 20)
+        
+        sut.refresh()
+        XCTAssertEqual(sut.storyViewModels.count, 10)
     }
 
+    func test_processStories(){
+        let multimedia = Multimedia(url: "/test/url/image.jpg")
+        let story = Story(snippet: "Test Snippet",
+                          multimedia: [multimedia],
+                          headline: Headline(main: "Test Headline"))
+        
+        let vms = sut.processStories(items: [story])
+        
+        XCTAssertEqual(vms[0].headline, story.headline.main)
+        XCTAssertEqual(vms[0].snippet, story.snippet)
+        XCTAssertEqual(vms[0].image, UIImage(named: AppConstants.imgUrl + story.multimedia[0].url))
+        
+    }
 }
