da0f273bc105cd5cefc36316e8e25d5ce877996c
jeff-tabios <jefftabios1@gmail.com>
2019-07-28 01:19:05 +0800

    Added app constants file

---
+++ JTStories/Application/AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstants.swift
+//  JTStories
+//
+//  Created by Jeff Tabios on 27/07/2019.
+//  Copyright © 2019 Jeff Tabios. All rights reserved.
+//
+
+import Foundation
+
+class AppConstant{
+    static let imgUrl = "https://static01.nyt.com/images"
+}
