1ce7601739d0a95ec966842708a8766e730635e2
jeff-tabios <jefftabios1@gmail.com>
2019-07-27 22:32:19 +0800

    Initial project files

---
+++ JTStories/Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JTStories
+//
+//  Created by Jeff Tabios on 27/07/2019.
+//  Copyright © 2019 Jeff Tabios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
