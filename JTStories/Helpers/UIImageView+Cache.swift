051d2c89bb8b28aa1a13a18de6746e5ae33985f3
jeff-tabios <jefftabios1@gmail.com>
2019-08-02 02:11:48 +0800

    Refactored UIImage caching to be testable

--- JTStories/Helpers/UIImageView+Cache.swift
+++ JTStories/Helpers/UIImageView+Cache.swift
@@ -9,16 +9,25 @@
 import Foundation
 import UIKit
 
+public enum ImageCachingStatus {
+    case none
+    case imageCached
+    case cachedImageUsed
+}
+
 extension UIImageView {
     /// Loads image from web asynchronosly and caches it, in case you have to load url
     /// again, it will be loaded from cache if available
-    func load(url: URL?, placeholder: UIImage?, cache: URLCache? = nil) {
+    func load(url: URL?, placeholder: UIImage?, cache: URLCache? = nil, completion: @escaping (ImageCachingStatus)->Void = {_ in }) {
         self.image = placeholder
         if let url = url {
-            let cache = cache ?? URLCache.shared
+            
+            let cache = URLCache.shared
             let request = URLRequest(url: url)
+            
             if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
                 self.image = image
+                completion(.cachedImageUsed)
             } else {
                 URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
                     if let data = data, let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300, let image = UIImage(data: data) {
@@ -26,10 +35,13 @@ extension UIImageView {
                         cache.storeCachedResponse(cachedData, for: request)
                         DispatchQueue.main.async {
                             self.image = image
+                            completion(.imageCached)
                         }
                     }
                 }).resume()
             }
+            
+            
         }
     }
 }
